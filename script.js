98b212dd22268fd18e38107403da97e2edfa82e7
Martin Rios <martin.rios@GRUPOMAIPU.LOCAL>
2026-05-12 17:21:05 -0300

    agrego planilla google

--- script.js
+++ script.js
@@ -336,6 +336,37 @@ function verificarClave() {
     }
 }
 
+// --- NUEVO: ENVÍO A GOOGLE SHEETS ---
+    const urlPlanilla = "https://script.google.com/macros/s/AKfycbx1g7yqChQFZ6Y9UzZ0iUONxjoiZ1-vGziUNva5Nnt5v-o8g0GOBjLefQ43OPDsOorr/exec"; // REEMPLAZA ESTO
+    const datosParaSheet = {
+        fecha: fecha,
+        hora: hora,
+        chofer: nombre,
+        legajo: legajo,
+        unidad: unidad,
+        patente: patente,
+        grupo: grupo,
+        linea: linea,
+        lat: lat,
+        lng: lng,
+        tercero: `${tNombre} (DNI: ${tDni}, Tel: ${tTel}, Vehículo: ${tMarca} ${tModelo})`,
+        lesionados: infoLesionados,
+        testigos: infotestigos,
+        relato: relato
+    };
+
+    try {
+        await fetch(urlPlanilla, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(datosParaSheet)
+        });
+    } catch (e) {
+        console.error("Error al guardar en Sheet", e);
+    }
+    // ---------------------------------------
+
 // Al cargar la página, revisar si ya estaba autorizado
 window.onload = function() {
     if (sessionStorage.getItem('autorizado') === 'true') {
